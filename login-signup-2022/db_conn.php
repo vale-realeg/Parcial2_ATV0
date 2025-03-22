9bc37cd0389c9dc3f69fdfd34654d5d665104133
vale-realeg <ia.valeria_realegeno@ufg.edu.sv>
2025-03-21 21:39:23 -0600

    Login y Registro php

---
+++ login-signup-2022/db_conn.php
@@ -0,0 +1,13 @@
+<?php 
+
+$sName = "localhost";
+$uName = "root";
+$pass = "";
+$db_name = "auth_db";
+
+try {
+    $conn = new PDO("mysql:host=$sName;port=3307;dbname=$db_name",$uName, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch(PDOException $e){
+  echo "Connection failed : ". $e->getMessage();
+}
